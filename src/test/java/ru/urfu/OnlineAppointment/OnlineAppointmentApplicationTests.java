b91ae3c43d8f3dab8c4f42b2b2ef050c80affe1c
eva gertseva <12gerts@bk.ru>
2022-10-19 20:25:55 +0300

    Add main files

---
+++ src/test/java/ru/urfu/OnlineAppointment/OnlineAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.OnlineAppointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
