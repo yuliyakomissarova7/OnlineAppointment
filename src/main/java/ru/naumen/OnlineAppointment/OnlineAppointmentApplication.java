5039b82075989ac173373eacbc05b6b15ad57c03
yulya <yliuakomissarova@gmail.com>
2022-11-02 21:50:43 +0500

    Init

---
+++ src/main/java/ru/naumen/OnlineAppointment/OnlineAppointmentApplication.java
@@ -0,0 +1,13 @@
+package ru.naumen.OnlineAppointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineAppointmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/naumen/OnlineAppointment/OnlineAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.naumen.OnlineAppointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
